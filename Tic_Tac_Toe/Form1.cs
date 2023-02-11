f9725eee5b6ff5a83de2f4aebae8f8b7407fb9e7
PB-03-faw7 <PB-03-faw7@DC1-S-TS33-011>
2023-02-11 15:32:55 +0100

    2

--- Tic_Tac_Toe/Form1.cs
+++ Tic_Tac_Toe/Form1.cs
@@ -34,109 +34,44 @@ namespace Tic_Tac_Toe
                 bt.Enabled = false;
                 lblXO.Text = "X";
             }
+            if ((button1.Text == "X" && button2.Text == "X" && button3.Text == "X" || 
+                (button4.Text == "X" && button5.Text == "X" && button6.Text == "X") ||
+                (button7.Text == "X" && button8.Text == "X" && button9.Text == "X") ||
+                (button1.Text == "X" && button4.Text == "X" && button7.Text == "X") ||
+                (button2.Text == "X" && button5.Text == "X" && button8.Text == "X") ||
+                (button3.Text == "X" && button6.Text == "X" && button9.Text == "X") ||
+                (button1.Text == "X" && button5.Text == "X" && button9.Text == "X") ||
+                (button3.Text == "X" && button5.Text == "X" && button7.Text == "X") ))
 
-            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X")
             {
                 MessageBox.Show("Der Gewinner X");
                 gameover();
             }
 
-            if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X")
-            {
-                MessageBox.Show("Der Gewinner X");
-                gameover();
-            }
-
-            if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X")
-            {
-                MessageBox.Show("Der Gewinner X");
-                gameover();
-            }
-
-            if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X")
-            {
-                MessageBox.Show("Der Gewinner X");
-                gameover();
-            }
-
-            if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X")
-            {
-                MessageBox.Show("Der Gewinner X");
-                gameover();
-            }
-
-            if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
-            {
-                MessageBox.Show("Der Gewinner X");
-                gameover();
-            }
-
-            if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X")
-            {
-                MessageBox.Show("Der Gewinner X");
-                gameover();
-            }
-
-            if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
-            {
-                MessageBox.Show("Der Gewinner X");
-                gameover();
-            }
             ///// 
 
-            if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O")
+            if (((button1.Text == "O" && button2.Text == "O" && button3.Text == "O" ||
+                (button4.Text == "O" && button5.Text == "O" && button6.Text == "O") ||
+                (button7.Text == "O" && button8.Text == "O" && button9.Text == "O") ||
+                (button1.Text == "O" && button4.Text == "O" && button7.Text == "O") ||
+                (button2.Text == "O" && button5.Text == "O" && button8.Text == "O") ||
+                (button3.Text == "O" && button6.Text == "O" && button9.Text == "O") ||
+                (button1.Text == "O" && button5.Text == "O" && button9.Text == "O") ||
+                (button3.Text == "O" && button5.Text == "O" && button7.Text == "O"))))
             {
                 MessageBox.Show("Der Gewinner O");
                 gameover();
             }
 
-            if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O")
-            {
-                MessageBox.Show("Der Gewinner O");
-                gameover();
-            }
-
-            if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O")
-            {
-                MessageBox.Show("Der Gewinner O");
-                gameover();
-            }
-
-            if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O")
-            {
-                MessageBox.Show("Der Gewinner O");
-                gameover();
-            }
-
-            if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O")
-            {
-                MessageBox.Show("Der Gewinner O");
-                gameover();
-            }
-
-            if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
-            {
-                MessageBox.Show("Der Gewinner O");
-                gameover();
-            }   
-
-            if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O")
-            {
-                MessageBox.Show("Der Gewinner O");
-                gameover();
-            }
-
-            if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
-            {
-                MessageBox.Show("Der Gewinner O");
-                gameover();
-            }
+            
             ///
             
-            //if (button1.Text !="" && button2.Text != "" && button3.Text != "" && button4.Text != "" && button5.Text != "" &&
-            //    button6.Text != "" && button7.Text != "" && button8.Text != "" && button9.Text != "")
-
-            if (button1.Enabled=false) 
+            if (button1.Enabled==false && button2.Enabled==false && button3.Enabled == false && button4.Enabled == false && button5.Enabled == false 
+                && button6.Enabled == false && button7.Enabled == false && button8.Enabled == false && button9.Enabled == false) 
+                
+                //(button1.Text !="" && button2.Text != "" && button3.Text != "" && button4.Text != "" && button5.Text != "" &&
+                //button6.Text != "" && button7.Text != "" && button8.Text != "" && button9.Text != "")
+                            
 
             {
                 MessageBox.Show("unentschieden");
